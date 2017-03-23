38a3b72dab948dba67609a88b53f0e765304c79c
RaghavSingh <raghavvishnoi10@gmail.com>
2017-03-23 13:28:29 +0530

    update location logic

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,2 +1,18 @@
 class Home < ActiveRecord::Base
+
+	reverse_geocoded_by :lat, :lng do |obj,results|
+	  if geo = results.first
+	    obj.city    = geo.city
+	    obj.zone = geo.state
+	    obj.country = geo.country_code
+	    obj.address = geo.address
+	  end
+	end
+	after_validation :reverse_geocode
+	 
+	 
+
+
+ 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323070629) do
+ActiveRecord::Schema.define(version: 20170323075732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170323070629) do
     t.string   "city"
     t.string   "zone"
     t.string   "name"
+    t.text     "address"
   end
 
 end
